3f799771b70424bdcb4c62763321bb8947c907bf
Maksym Andreiev <maxoggyha@gmail.com>
2024-03-18 03:10:26 +0900

    Refactor adding participants func

--- processScrap.py
+++ processScrap.py
@@ -16,6 +16,50 @@ class Episode(db.Model):
     __table__ = db.Model.metadata.tables['main.Episodes']
 
 
+def add_participant(team, sex_label):
+    ids = []
+    for team_member in team:
+        if team_member == 'Аліна Завальська':
+            team_member = 'Ангеліна Завальська'
+        elif team_member == '​​Даша Малахова':
+            team_member = 'Дарія Малахова'
+        elif team_member == 'Тереза ​​Балашова':
+            team_member = 'Тереза Балашова'
+        elif team_member == 'Анна "Ахава" Тесленко':
+            team_member = 'Анна Тесленко'
+        team_member = team_member.replace('  ', ' ')
+        participants = Participant.query.filter_by(name=team_member).all()
+        if len(participants) == 1:  # found before
+            participant = participants[0]
+            if team_member == team[-1]:  # the last member of team is always a capitan
+                if participant.capitan == 0:  # if he was before as participant
+                    new_participant = Participant(name=team_member, sex=sex_label,
+                                                  capitan=1)  # add him as a new participant as capitan
+                    db.session.add(new_participant)
+                    db.session.commit()
+                    participant = Participant.query.filter_by(name=team_member).first()
+            participant_id = participant.ID
+            ids.append(participant_id)
+        elif len(participants) == 0:
+            capitan_label = 0
+            if team_member == team[-1]:  # capitan
+                capitan_label = 1
+            new_participant = Participant(name=team_member, sex=sex_label, capitan=capitan_label)
+            db.session.add(new_participant)
+            db.session.commit()
+            participant = Participant.query.filter_by(name=team_member).first()
+            participant_id = participant.ID
+            ids.append(participant_id)
+        elif len(participants) == 2:
+            if team_member == team[-1]:
+                participant = Participant.query.filter_by(name=team_member, capitan=1).first()
+            else:
+                participant = Participant.query.filter_by(name=team_member, capitan=0).first()
+            participant_id = participant.ID
+            ids.append(participant_id)
+    return ids
+
+
 with open('scrapped_data.csv', newline='\n') as csvfile:
     episode_reader = csv.reader(csvfile)
     next(episode_reader)
@@ -25,61 +69,15 @@ with open('scrapped_data.csv', newline='\n') as csvfile:
         date = datetime.datetime.strptime(row[2], '%Y-%m-%d')
         men = row[3].split(';')
         women = row[6].split(';')
-        men_id = []
-        women_id = []
-        for man in men:
-            participants = Participant.query.filter_by(name=man).all()
-            if len(participants) == 1:
-                participant = participants[0]
-                if man == men[-1]:
-                    if participant.capitan == 0:
-                        male_participant = Participant(name=man, sex=1, capitan=1)
-                        db.session.add(male_participant)
-                        participant = Participant.query.filter_by(name=man).first()
-                participant_id = participant.ID
-                men_id.append(participant_id)
-            elif len(participants) == 0:
-                capitan_label = 0
-                if man == men[-1]:  # capitan
-                    capitan_label = 1
-                male_participant = Participant(name=man, sex=1, capitan=capitan_label)
-                db.session.add(male_participant)
-                participant = Participant.query.filter_by(name=man).first()
-                participant_id = participant.ID
-                men_id.append(participant_id)
-        print(men_id)
-        for woman in women:
-            if woman == 'Аліна Завальська':
-                woman = 'Ангеліна Завальська'
-            elif woman == '​​Даша Малахова':
-                woman = 'Дарія Малахова'
-            participants = Participant.query.filter_by(name=woman).all()
-            if len(participants) == 1:
-                participant = participants[0]
-                if woman == women[-1]:
-                    if participant.capitan == 0:
-                        female_participant = Participant(name=woman, sex=1, capitan=1)
-                        db.session.add(female_participant)
-                        participant = Participant.query.filter_by(name=woman).first()
-                participant_id = participant.ID
-                women_id.append(participant_id)
-            elif len(participants) == 0:
-                capitan_label = 0
-                if woman == women[-1]:  # capitan
-                    capitan_label = 1
-                female_participant = Participant(name=woman, sex=0, capitan=capitan_label)
-                db.session.add(female_participant)
-                db.session.commit()
-                participant = Participant.query.filter_by(name=woman).first()
-                participant_id = participant.ID
-                women_id.append(participant_id)
+        men_id = add_participant(men, 1)
+        women_id = add_participant(women, 0)
         if len(men_id) == 4 and len(women_id) == 4:
             episode_data = Episode(season=season, date=date, male1=men_id[0], female1=women_id[0],
                                    female2=women_id[1], female3=women_id[2], female_capitan=women_id[3],
                                    male2=men_id[1], male3=men_id[2], male_capitan=men_id[3])
         else:
             for i in range(len(men_id)):
-                man =Participant.query.filter_by(ID=men_id[i]).first()
+                man = Participant.query.filter_by(ID=men_id[i]).first()
                 print(man.name)
             for i in range(len(women_id)):
                 woman = Participant.query.filter_by(ID=women_id[i]).first()
